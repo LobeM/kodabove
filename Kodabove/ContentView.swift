76c01a963850af3e48f5bc09bc43883109993d12
Lobe Musonda <lobe350@gmail.com>
2022-09-24 07:47:56 +0200

    Initial Commit

---
+++ Kodabove/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Kodabove
+//
+//  Created by Lobe Musonda on 9/24/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Kodabove/KodaboveApp.swift
@@ -0,0 +1,17 @@
+//
+//  KodaboveApp.swift
+//  Kodabove
+//
+//  Created by Lobe Musonda on 9/24/22.
+//
+
+import SwiftUI
+
+@main
+struct KodaboveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
