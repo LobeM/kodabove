4ebd2c01945526e75ff700b7fc055257df0b58e9
Lobe Musonda <lobe350@gmail.com>
2022-09-26 11:07:20 +0200

    Formated dates in schedule

--- Kodabove/models/Schedule.swift
+++ Kodabove/models/Schedule.swift
@@ -14,3 +14,35 @@ struct Schedule: Codable, Identifiable {
     var date: String
     var imageUrl: String
 }
+
+extension Schedule {
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.id = try container.decode(String.self, forKey: .id)
+        self.title = try container.decode(String.self, forKey: .title)
+        self.subtitle = try container.decode(String.self, forKey: .subtitle)
+        
+        if let apiDate = try container.decodeIfPresent(String.self, forKey: .date) {
+            let relativeDateFormatter = DateFormatter()
+            relativeDateFormatter.timeStyle = .short
+            relativeDateFormatter.dateStyle = .short
+            relativeDateFormatter.locale = Locale(identifier: "en_GB")
+            relativeDateFormatter.doesRelativeDateFormatting = true
+            
+            let inputFormatter = DateFormatter()
+            inputFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZ"
+            let date = inputFormatter.date(from: apiDate)
+            
+            if let newDate = date {
+                self.date = relativeDateFormatter.string(from: newDate)
+            } else {
+                self.date = ""
+            }
+            
+        } else {
+            self.date = ""
+        }
+        
+        self.imageUrl = try container.decode(String.self, forKey: .imageUrl)
+    }
+}
