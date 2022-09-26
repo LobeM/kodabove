ae45704ade8e9a3fe0cab06a39c753b706b987f3
Lobe Musonda <lobe350@gmail.com>
2022-09-26 19:55:41 +0200

    Reloading schedule data every 30s

--- Kodabove/network/NetworkManager.swift
+++ Kodabove/network/NetworkManager.swift
@@ -5,19 +5,28 @@
 //  Created by Lobe Musonda on 9/26/22.
 //
 
-import Foundation
+import SwiftUI
+import Combine
 
 class NetworkManager: ObservableObject {
     @Published var events: [Event] = []
     @Published var schedules: [Schedule] = []
     @Published var loading: Bool = false
     
+    private var timer: Timer?
     private let baseUrl = "https://us-central1-dazn-sandbox.cloudfunctions.net"
     
     init() {
         loading = true;
         getEvents()
         getSchedules()
+        timer = Timer.scheduledTimer(withTimeInterval: 30, repeats: true, block: { _ in
+            self.getSchedules()
+        })
+    }
+    
+    deinit {
+        timer?.invalidate()
     }
     
     private func getEvents() {
@@ -31,7 +40,6 @@ class NetworkManager: ObservableObject {
             let events = try! JSONDecoder().decode([Event].self, from: data)
             
             DispatchQueue.main.async {
-//                completion(events)
                 self.events = events
                 self.loading = false
             }
@@ -39,7 +47,7 @@ class NetworkManager: ObservableObject {
         .resume()
     }
     
-    func getSchedules() {
+    private func getSchedules() {
         guard let url = URL(string: "\(baseUrl)/getSchedule") else {
             print("API not found")
             return
@@ -50,9 +58,7 @@ class NetworkManager: ObservableObject {
             let schedules = try! JSONDecoder().decode([Schedule].self, from: data)
             
             DispatchQueue.main.async {
-//                completion(schedule)
                 self.schedules = schedules
-                self.loading = false
             }
         }
         .resume()
