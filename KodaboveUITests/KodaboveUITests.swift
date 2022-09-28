02e9c08ee64ab0a5873db474e9109ce66a8d3774
Lobe Musonda <lobe350@gmail.com>
2022-09-28 10:24:34 +0200

    Implemented UI tests

--- KodaboveUITests/KodaboveUITests.swift
+++ KodaboveUITests/KodaboveUITests.swift
@@ -8,34 +8,69 @@
 import XCTest
 
 final class KodaboveUITests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
-        continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
+    var app: XCUIApplication!
+    
+    override func setUp() {
+        app = XCUIApplication()
         app.launch()
-
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
-
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
+    
+    override func tearDown() {
+        app = nil
+    }
+    
+    func test_open_events_tab() {
+        app.tabBars.buttons["Events"].tap()
+        XCTAssert(app.navigationBars["Events"].exists)
+    }
+    
+    func test_open_schedule_tab() {
+        app.tabBars.buttons["Schedule"].tap()
+        XCTAssert(app.navigationBars["Schedule"].exists)
+    }
+    
+    func test_schedule_list_is_loading() {
+        app.tabBars.buttons["Schedule"].tap()
+        XCTAssert(app.navigationBars["Schedule"].exists)
+        
+        let scheduleList = app.otherElements["scheduleList"]
+        XCTAssertTrue(scheduleList.waitForExistence(timeout: 5), "The List should be visible")
+    }
+    
+    func test_select_event_item() {
+        app.tabBars.buttons["Events"].tap()
+        XCTAssert(app.navigationBars["Events"].exists)
+        
+        let eventList = app.otherElements["eventsList"]
+        XCTAssertTrue(eventList.waitForExistence(timeout: 5), "The List should be visible")
+        
+        let eventCount = eventList.buttons.count
+        XCTAssertTrue(eventCount > 0)
+        
+        let firstEvent = eventList.buttons.element(boundBy: 0)
+        XCTAssertTrue(firstEvent.exists)
+        firstEvent.tap()
+        
+        XCTAssert(app.navigationBars["Playback"].exists)
+    }
+    
+    func test_back_button_from_playback() {
+        app.tabBars.buttons["Events"].tap()
+        XCTAssert(app.navigationBars["Events"].exists)
+        
+        let eventList = app.otherElements["eventsList"]
+        XCTAssertTrue(eventList.waitForExistence(timeout: 5), "The List should be visible")
+        
+        let eventCount = eventList.buttons.count
+        XCTAssertTrue(eventCount > 0)
+        
+        let firstEvent = eventList.buttons.element(boundBy: 0)
+        XCTAssertTrue(firstEvent.exists)
+        firstEvent.tap()
+        
+        XCTAssert(app.navigationBars["Playback"].exists)
+        
+        app.navigationBars.buttons.element(boundBy: 0).tap()
+        XCTAssert(app.navigationBars["Events"].exists)
     }
 }
